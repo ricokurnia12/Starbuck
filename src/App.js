959f0998ed690f6f172f9c3eda79eb5197e29661
bafir24 <bafirgenius@gmail.com>
2022-11-24 16:47:13 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+import Home from "./Home";
+import { NavbarItems1 } from "./components/NavbarItems";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Home />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/NavbarItems.js
@@ -0,0 +1,86 @@
+import React from "react";
+import "./navbaritems.css";
+
+export const NavbarItems1 = () => {
+    return (
+        <div className="container-navbaritems container text-center">
+            <div className="row">
+                <div className="col-lg-2">
+                    <h3>item 1</h3>
+                    <ul className="list-unstyled">
+                        <li>item1.1</li>
+                        <li>item1.2</li>
+                        <li>item1.3</li>
+                        <li>item1.4</li>
+                        <li>item1.5</li>
+                        <li>item1.6</li>
+                    </ul>
+                </div>
+                <div className="col-lg-2">
+                    <h3>item 2</h3>
+                    <ul className="list-unstyled">
+                        <li>item1.1</li>
+                        <li>item1.2</li>
+                        <li>item1.3</li>
+                        <li>item1.4</li>
+                        <li>item1.5</li>
+                        <li>item1.6</li>
+                    </ul>
+                </div>
+                <div className="col-lg-2">
+                    <h3>item 3</h3>
+                    <ul className="list-unstyled">
+                        <li>item1.1</li>
+                        <li>item1.2</li>
+                        <li>item1.3</li>
+                        <li>item1.4</li>
+                        <li>item1.5</li>
+                        <li>item1.6</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export const NavbarItems2 = () => {
+    return (
+        <div className="container-navbaritems container text-center">
+            <div className="row">
+                <div className="col-lg-2">
+                    <h3>item 1</h3>
+                    <ul className="list-unstyled">
+                        <li>item1.1</li>
+                        <li>item1.2</li>
+                        <li>item1.3</li>
+                        <li>item1.4</li>
+                        <li>item1.5</li>
+                        <li>item1.6</li>
+                    </ul>
+                </div>
+                <div className="col-lg-2">
+                    <h3>item 2</h3>
+                    <ul className="list-unstyled">
+                        <li>item1.1</li>
+                        <li>item1.2</li>
+                        <li>item1.3</li>
+                        <li>item1.4</li>
+                        <li>item1.5</li>
+                        <li>item1.6</li>
+                    </ul>
+                </div>
+                <div className="col-lg-2">
+                    <h3>item 3</h3>
+                    <ul className="list-unstyled">
+                        <li>item1.1</li>
+                        <li>item1.2</li>
+                        <li>item1.3</li>
+                        <li>item1.4</li>
+                        <li>item1.5</li>
+                        <li>item1.6</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
